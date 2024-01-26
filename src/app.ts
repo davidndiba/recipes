dba65daf30b9b9b4b44872bd8ffc9ba0dba6af27
David <david.ndiba@student.moringaschool.com>
2024-01-26 11:24:44 +0300

    done with sidebar and header

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,3 @@
-// // 运行时配置
-
-// // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
-// // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
-// export async function getInitialState(): Promise<{ name: string }>{
-//  return { name: 'RecipesCorn'}
-// }
-
-// export const layout = () => {
-//   return {
-//     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6t9bwUGbQKqnl851ItOiEGGtSw6_zlJ4gyfvcnvbGPbL6-Spn',
-//     menu: {
-//       layout: false,
-//     },
-//   };
-// };
-// 运行时配置
-
-// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
-// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: 'RecipesCorn' };
 }
@@ -27,5 +7,10 @@ export const layout = () => {
       layout: false, 
     headerRender: false, 
     menuRender: false,
+    footerRender: false,
+    menuHeaderRender: false,
+    // hideChildrenInMenu=true,
+    // hideInMenu=true,
+    // hideInBreadcrumb=true
   };
 };
